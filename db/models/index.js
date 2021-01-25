7b0860dad33e683491310eda97930238c3313b0c
Sam Kessler <sholomkessler1@gmail.com>
2021-01-25 17:19:22 -0500

    Defined district layer data type

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,9 @@
 const Player = require('./player');
+const DistrictLayer = require('./districtlayer');
 
 //ASSOICATIONS GO HERE -- Read more at https://sequelize.org/master/manual/assocs.html
+//DistrictLayer.hasMany(Maps, {as:'maps'})
 
 module.exports = {
-  Player,
+	Player,
 };
