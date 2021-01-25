df779051a9d8e61d08f0237c87fc423a09fed0ea
Sam Kessler <sholomkessler1@gmail.com>
2021-01-25 18:05:34 -0500

    Readded dotenv and migrated sequelize to env params

--- db/db.js
+++ db/db.js
@@ -1,29 +1,22 @@
 const Sequelize = require('sequelize');
 const { name } = require('../package.json');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Initialize database with Sequelize
-/*const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${name}`,
-  {
-    logging: false,
-  }
-);
-*/
 
-const db = new Sequelize('electionmapbackend', 'postgres', 'palkia1', {
-  host: 'localhost',
-  dialect: 'postgres',
+const db = new Sequelize(name, process.env.DATABASE_USERNAME || 'postgress', process.env.DATABASE_PASSWORD, {
+	host: process.env.DATABASE_IP || 'localhost',
+	dialect: 'postgres',
+	port: process.env.DATABASE_PORT || 5432,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-
-  
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+	logging: false,
 });
-
-
 
 module.exports = db;
