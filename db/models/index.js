6202ea65cb895c34b6760f5aed5310433ca6ecaf
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2021-01-25 17:23:34 -0500

    Defined Candidate layer data type

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,9 @@
 const Player = require('./player');
+const Candidate = require('./candidate');
 
 //ASSOICATIONS GO HERE -- Read more at https://sequelize.org/master/manual/assocs.html
 
 module.exports = {
   Player,
+  Candidate,
 };
