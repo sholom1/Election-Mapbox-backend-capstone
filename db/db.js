96cf8312669b4a82b07c55a853135ec112c1b1ba
Youness Elwiddi <ywiddi1020@gmail.com>
2021-01-25 17:48:57 -0500

    Changed dbjs

--- db/db.js
+++ db/db.js
@@ -1,11 +1,29 @@
-const dotenv = require('dotenv');
-dotenv.config();
 const Sequelize = require('sequelize');
 const { name } = require('../package.json');
 
 // Initialize database with Sequelize
-const db = new Sequelize((process.env.DATABASE_URL || 'postgres://localhost:5432/') + name, {
-	logging: false,
+/*const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${name}`,
+  {
+    logging: false,
+  }
+);
+*/
+
+const db = new Sequelize('electionmapbackend', 'postgres', 'palkia1', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  
 });
+
+
 
 module.exports = db;
