539a6f54c4131621655da14aa491088708f69999
Sam Kessler <sholomkessler1@gmail.com>
2021-01-26 14:05:39 -0500

    Implemented create layer expression
    needs testing

--- db/models/candidate.js
+++ db/models/candidate.js
@@ -2,16 +2,20 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Candidate = db.define('candidate', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
 
-  votes: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
+	votes: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
 
+	color: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
 });
 
 module.exports = Candidate;
