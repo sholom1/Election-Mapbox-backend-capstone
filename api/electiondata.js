4a315a1366b9ac22d405b516dd19abc86ad46406
Sam Kessler <sholomkessler1@gmail.com>
2021-01-25 19:17:54 -0500

    ElectionData routes completed

--- api/electiondata.js
+++ api/electiondata.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { District, DistrictLayer, ElectionData } = require('../db/models');
+const { ElectionData } = require('../db/models');
 
 // Express Routes for election data files - Read more on routing at https://expressjs.com/en/guide/routing.html
-router.get('/', async (req, res, next) => {
-	try {
-		const allPlayers = await Player.findAll();
-		// An if/ternary statement to handle not finding allPlayers explicitly
-		!allPlayers ? res.status(404).send('Players Listing Not Found') : res.status(200).json(allPlayers);
-	} catch (error) {
-		next(error);
-	}
+router.get('/', (req, res, next) => {
+	ElectionData.findAll()
+		.then((sheets) => res.json(sheets))
+		.catch((err) => next(err));
+});
+router.get('/:id', (req, res, next) => {
+	ElectionData.findByPk(req.params.id)
+		.then((sheet) => res.json(sheet))
+		.catch((err) => next(err));
 });
 router.post('/', (req, res, next) => {
 	ElectionData.create({
