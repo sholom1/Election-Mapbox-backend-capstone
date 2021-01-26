c04036902aff6b22cd1e92084797084d30dae20f
Sam Kessler <sholomkessler1@gmail.com>
2021-01-25 19:09:31 -0500

    Added name to district layer

--- db/models/districtlayer.js
+++ db/models/districtlayer.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const DistrictLayer = db.define('District Layer', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
 	data: {
 		type: Sequelize.JSONB,
 		allowNull: false,
