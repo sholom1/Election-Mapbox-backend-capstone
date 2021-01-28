754ed1806facd0d0ba80ec57beddc77d29529c72
Sam Kessler <sholomkessler1@gmail.com>
2021-01-27 19:35:25 -0500

    Now catching db sync

--- index.js
+++ index.js
@@ -47,7 +47,13 @@ const serverRun = () => {
 // {force:true} - drops current tables and places new empty tables
 //{alter:true} - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 
-const syncDb = () => db.sync({ force: false, alter: true });
+const syncDb = async () => {
+	try {
+		await db.sync({ force: false, alter: true });
+	} catch (err) {
+		console.log(err);
+	}
+};
 // Connects to //postgres://localhost:5432/dbname
 
 //Run server and sync DB
