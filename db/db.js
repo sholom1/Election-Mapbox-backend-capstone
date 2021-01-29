ca00e35ccfb8831961084e94b8de8f8e772b44e8
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2021-01-29 16:50:02 -0500

    fixed connection with heroku

--- db/db.js
+++ db/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 // Initialize database with Sequelize
 
+// --------------------------THIS SECTION OF CODE IS NO LONGER NECESSARY-----------------------------------
 // const db = new Sequelize(name, process.env.DATABASE_USERNAME || 'postgress', process.env.DATABASE_PASSWORD, {
 // 	host: process.env.DATABASE_IP || 'localhost',
 // 	dialect: 'postgres',
@@ -18,16 +19,24 @@ dotenv.config();
 // 	},
 // 	logging: false,
 // });
+//----------------------------------------------------------------------------------------------------
 
+/* 
+WHEN RUNNING WITH LOCAL DATABASE, .env FILE MUST CONTAIN:  
+DATABASE_URL = 'postgres://<username>:<password>@localhost:5432/electionmapbackend' 
+*/
 const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            sslmode: 'require',
-            rejectUnauthorized: false
-        }
-    }
+	protocol: 'postgres',
+	
+	//-----THIS SECTION MUST BE ENABLED WHEN RUNNING WITH HEROKU DATABASE, BUT DISABLED WHEN RUNNING WITH LOCAL DATABASE-------------
+    // dialectOptions: {
+    //     ssl: {
+    //         sslmode: 'require',
+    //         rejectUnauthorized: false
+	// 	}	
+	// }
+	//---------------------------------------------------------------------------------------
 });
 
 module.exports = db;
