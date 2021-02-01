7dc7ee99106238faf90a596407e3038724c0909c
Sam Kessler <sholomkessler1@gmail.com>
2021-02-01 17:02:26 -0500

    Removed associations

--- api/composers/generateMap.js
+++ api/composers/generateMap.js
@@ -7,18 +7,22 @@ const LayerExpressions = require('./createlayerexpressions');
 const generateMap = async (name, sheets, districtLayer, colordata, category) => {
 	//console.log(DistrictCandidateObject);
 	let results = DistrictCandidateObject(sheets); //new DistrictCandidateObject(sheets);
-	let districts = await createDistricts(results, colordata);
+	//let districts = await createDistricts(results, colordata);
 	let geoJson = districtLayer.dataValues.data;
-	let { colorExpression, opacityExpression } = new LayerExpressions(results, geoJson.features, colordata);
+	let { colorExpression, opacityExpression } = new LayerExpressions(
+		results,
+		geoJson,
+		colordata,
+	);
 	ElectionMap.create({
 		name,
 		layers: { colorExpression, opacityExpression },
 		geoJson,
 		districts: results,
 		category,
-	}).then((result) => {
-		result.setDistricts(districts);
-		result.setGeojson(geojson);
 	});
+	// 	.then((result) => {
+	// 	result.setDistricts(districts);
+	// });
 };
 module.exports = generateMap;
--- db/models/index.js
+++ db/models/index.js
@@ -11,10 +11,10 @@ const Category = require('./category');
 //ASSOICATIONS GO HERE -- Read more at https://sequelize.org/master/manual/assocs.html
 DistrictLayer.hasMany(ElectionMap, { as: 'Maps' });
 ElectionMap.belongsTo(DistrictLayer, { as: 'Geojson' });
-District.hasMany(Candidate, { as: 'Candidates' });
-Candidate.belongsTo(District);
-ElectionMap.hasMany(District, { as: 'Districts' });
-District.belongsTo(ElectionMap);
+//District.hasMany(Candidate, { as: 'Candidates' });
+//Candidate.belongsTo(District);
+//ElectionMap.hasMany(District, { as: 'Districts' });
+//District.belongsTo(ElectionMap);
 
 module.exports = {
 	Player,
