54a0ef68de19172c1bf5f08df4946b46abc661cd
Sam Kessler <sholomkessler1@gmail.com>
2021-02-03 20:36:44 -0500

    Moved cors to env variable
    Added freezetablename to category table

--- db/models/category.js
+++ db/models/category.js
@@ -1,12 +1,18 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Category = db.define('Category', {
-	name: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		isEmpty: false,
+const Category = db.define(
+	'Category',
+	{
+		name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			isEmpty: false,
+		},
 	},
-});
+	{
+		freezeTableName: true,
+	}
+);
 
 module.exports = Category;
